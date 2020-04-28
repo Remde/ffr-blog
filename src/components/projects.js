373279f987df2a2f5a961a852cb4238ca82b2969
Fernando Remde <fernandoremde@Fernandos-MacBook-Pro.local>
2020-04-28 18:44:14 -0300

    projects increase on hover

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,18 +9,19 @@ export default () =>
 
   <div className={containerStyles.projectsItem}>
   <a href="https://github.com/Remde/fernandoremde-website" target="_blank">
-    <img src={Projectimg} alt="Project 1" />
+    <img className={containerStyles.imagem} src={Projectimg} alt="Project 1" />
     <div className={containerStyles.projectsItemText}>
       <h2>website and blog</h2>
       <p>fernandoremde.com is an open source responsive website made with the React framework and built using Gatsby.</p>
     </div>
+
   </a>
   </div>
 
 
   <div className={containerStyles.projectsItem}>
   <a href="https://github.com/Remde/BTOWEEN" target="_blank">
-    <img src={Projectimg} alt="Project 1" />
+    <img className={containerStyles.imagem} src={Projectimg} alt="Project 1" />
     <div className={containerStyles.projectsItemText}>
       <h2>music player</h2>
       <p>BTOWEEN is a music player made with Java in which you can specify musical notes in the key of C and it plays back to you.</p>
@@ -29,7 +30,7 @@ export default () =>
   </div>
 
   <div className={containerStyles.projectsItem}>
-    <Link to="/"><img src={Projectimg} alt="Project 1" /></Link>
+    <Link to="/"><img className={containerStyles.imagem} src={Projectimg} alt="Project 1" /></Link>
     <div className={containerStyles.projectsItemText}>
       <h2>project title</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec bibendum nulla.</p>
@@ -37,7 +38,7 @@ export default () =>
   </div>
 
   <div className={containerStyles.projectsItem}>
-    <Link to="/"><img src={Projectimg} alt="Project 1" /></Link>
+    <Link to="/"><img className={containerStyles.imagem} src={Projectimg} alt="Project 1" /></Link>
     <div className={containerStyles.projectsItemText}>
       <h2>project title</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
