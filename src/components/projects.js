cb4a6aba6e4bf766677e24ec6397fdf08d97b82b
remde <ffremde@gmail.com>
2021-03-10 19:38:02 -0300

    Update projects area

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,56 +1,51 @@
 import React from "react"
 import containerStyles from "./projects.module.css"
-import Picsimg from "../images/cars.png"
 import Blogimg from "../images/blog.png"
-import Noteimg from "../images/note.png"
 import Gardenimg from "../images/garden.png"
+import Myfeedimg from "../images/myfeed.png"
+import Smbimg from "../images/supermercadobot.png"
 
 export default () =>
 <div className={containerStyles.projects} id="projects">
 
-
   <div className={containerStyles.projectsItem}>
-  <a href="https://github.com/Remde/fernandoremde-website" rel="noopener noreferrer" target="_blank">
+  <a href="https://github.com/remde/fernandoremde-website" rel="noopener noreferrer" target="_blank">
     <img className={containerStyles.imagem} src={Blogimg} alt="Project 1" />
     <div className={containerStyles.projectsItemText}>
       <h2>website and blog</h2>
       <p>fernandoremde.com is an open source responsive website made with the React framework and built using Gatsby.</p>
     </div>
-
   </a>
   </div>
 
-
   <div className={containerStyles.projectsItem}>
-  <a href="https://github.com/Remde/BTOWEEN" rel="noopener noreferrer" target="_blank">
-    <img className={containerStyles.imagem} src={Noteimg} alt="Project 1" />
+  <a href="https://github.com/remde/myfeed" rel="noopener noreferrer" target="_blank">
+    <img className={containerStyles.imagem} src={Myfeedimg} alt="Project 1" />
     <div className={containerStyles.projectsItemText}>
-      <h2>music player</h2>
-      <p>BTOWEEN is a music player made with Java in which you can specify musical notes in the key of C and it plays back to you.</p>
+      <h2>myfeed</h2>
+      <p>myfeed is a way to get lobste.rs and hackernews articles straight from the terminal. Developed in Golang.</p>
     </div>
   </a>
   </div>
 
   <div className={containerStyles.projectsItem}>
-  <a href="https://github.com/Remde/react-pics" rel="noopener noreferrer" target="_blank">
-    <img className={containerStyles.imagem} src={Picsimg} alt="Project 1" />
-    <div className={containerStyles.projectsItemText}>
-      <h2>pics search</h2>
-      <p>Pics is a project made with React that fetches images with axios and display them on screen adjusting the divs to their sizes.</p>
-    </div>
-    </a>
-  </div>
-
-  <div className={containerStyles.projectsItem}>
-  <a href="https://github.com/Remde/gardn" rel="noopener noreferrer" target="_blank">
+  <a href="https://github.com/remde/gardn" rel="noopener noreferrer" target="_blank">
     <img className={containerStyles.imagem} src={Gardenimg} alt="Project 1" />
     <div className={containerStyles.projectsItemText}>
       <h2>gardn</h2>
-      <p>Gardn is an anonymous web message app based on React. Back-end was built using Node.js and Express.</p>
+      <p>Gardn is an anonymous web message app based on React. Back-end was developed using Node.js and Express.</p>
     </div>
     </a>
   </div>
 
-
+  <div className={containerStyles.projectsItem}>
+  <a href="https://github.com/remde/supermercadobot" rel="noopener noreferrer" target="_blank">
+    <img className={containerStyles.imagem} src={Smbimg} alt="Project 1" />
+    <div className={containerStyles.projectsItemText}>
+      <h2>super mercado bot</h2>
+      <p>Super Mercado Bot is a Telegram Bot developed in node.js that manages grocery shopping.</p>
+    </div>
+    </a>
+  </div>
 
 </div>
